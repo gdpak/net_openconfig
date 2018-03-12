531982d44e24c302ebf748dacd1cd24497083ac2
Deepak Agrawal <deepacks@gmail.com>
2018-03-12 10:16:55 +0000

    support common bgp params

--- tests/native_ncclient.py
+++ tests/native_ncclient.py
@@ -1,6 +1,7 @@
 from ncclient import manager
 
-with manager.connect(host="11.1.1.3", port=830, username="vagrant", password="vagrant", hostkey_verify=False) as m:
+with manager.connect(host="vsrx02.example.net", port=830,
+        username="ansible", password="Ansible", hostkey_verify=False) as m:
     c = m.get_config(source='running').data_xml
     print (c)
     with open("%s.xml" % host, 'w') as f:
