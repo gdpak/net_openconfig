14e5d453c73bca5da1b937d21a71c05fb98b6dfe
Deepak Agrawal <deepacks@gmail.com>
2018-03-16 06:56:11 +0000

    xml tree for junos and iosxr

--- tests/native_ncclient.py
+++ tests/native_ncclient.py
@@ -1,8 +1,12 @@
 from ncclient import manager
 
-with manager.connect(host="vsrx02.example.net", port=830,
+with manager.connect(host="vsrx01.example.net", port=830,
         username="ansible", password="Ansible", hostkey_verify=False) as m:
     c = m.get_config(source='running').data_xml
     print (c)
-    with open("%s.xml" % host, 'w') as f:
-        f.write(c)
+    '''
+    for cap in m.server_capabilities:
+        print (cap)a
+    '''
+    #with open("%s.xml" % host, 'w') as f:
+    #    f.write(c)
